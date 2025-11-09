f50e1914a3e9bb55559daa945967dcce9434b65a
Arnaud Kpai <arianhokpai@icloud.com>
2025-11-09 21:23:43 +0000

    feat: ajout des icônes dans drawable et mise à jour de la carte de visite

--- app/src/main/java/com/example/cartedevisite/MainActivity.kt
+++ app/src/main/java/com/example/cartedevisite/MainActivity.kt
@@ -6,10 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,7 +26,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -95,10 +103,10 @@ fun BusinessCard() {
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Divider(
-                color = Color(0xFFB2DFDB),
+            HorizontalDivider(
+                modifier = Modifier.padding(horizontal = 16.dp),
                 thickness = 1.dp,
-                modifier = Modifier.padding(horizontal = 16.dp)
+                color = Color(0xFFB2DFDB)
             )
 
             Spacer(modifier = Modifier.height(16.dp))
